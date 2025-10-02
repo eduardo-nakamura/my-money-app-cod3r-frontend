b24875bc50cd9b84b3d04e9f4d7899cfb42c8f5d
Eduardo Nakamura <50833822+eduardo-nakamura@users.noreply.github.com>
2025-10-02 18:47:29 -0300

    integração redux dashboard

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 import ReactDOM from 'react-dom'
 import App from './main/app'
+import { createStore, applyMiddleware } from "redux";
+import {Provider } from 'react-redux'
+import reducers from './main/reducers'
+import promise from 'redux-promise'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const store = applyMiddleware(promise)(createStore)(reducers)
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    
+, document.getElementById('app'))
