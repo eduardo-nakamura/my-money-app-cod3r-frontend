c56de6682e81163e7867d4a6386ad587f112af88
Eduardo Nakamura <50833822+eduardo-nakamura@users.noreply.github.com>
2025-10-03 15:14:07 -0300

    criacao tabs

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react'
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+import Tabs from '../common/tab/tabs'
+import TabsHeader from '../common/tab/tabsHeader'
+import TabHeader from '../common/tab/tabHeader'
+import TabsContent from '../common/tab/tabsContent'
+export default class billingCycle extends Component {
+  render() {
+    return (
+      <div>
+        <ContentHeader title="Ciclos de Pagamentos" small="Cadastro" />
+        <Content>
+            <Tabs>
+                <TabsHeader>
+                    <TabHeader label='Listar' icon='bars' target='tabList' />
+                    <TabHeader label='Incluir' icon='plus' target='tabCreate' />
+                    <TabHeader label='Alterar' icon='pencil' target='tabUpdate' />
+                    <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
+                </TabsHeader>
+                <TabsContent>
 
-export default props => (
-    <div>
-        <h1>Ciclo de Pagamento</h1>
-    </div>
-)
+                </TabsContent>
+            </Tabs>
+        </Content>
+      </div>
+    )
+  }
+}
