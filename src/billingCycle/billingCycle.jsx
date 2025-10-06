63e9630b17c4ec9af27e5ab5574de922954a9911
Eduardo Nakamura <50833822+eduardo-nakamura@users.noreply.github.com>
2025-10-06 15:54:28 -0300

    tela cadastro ciclos de pagamentos

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -1,29 +1,55 @@
 import React, { Component } from 'react'
+
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 import Tabs from '../common/tab/tabs'
 import TabsHeader from '../common/tab/tabsHeader'
 import TabHeader from '../common/tab/tabHeader'
 import TabsContent from '../common/tab/tabsContent'
-export default class billingCycle extends Component {
+import TabContent from '../common/tab/tabContent'
+
+import { selectTab } from '../common/tab/tabAction'
+
+class billingCycle extends Component {
+  componentWillMount(){
+    this.props.selectTab('tabList')
+  }
   render() {
     return (
       <div>
         <ContentHeader title="Ciclos de Pagamentos" small="Cadastro" />
         <Content>
-            <Tabs>
-                <TabsHeader>
-                    <TabHeader label='Listar' icon='bars' target='tabList' />
-                    <TabHeader label='Incluir' icon='plus' target='tabCreate' />
-                    <TabHeader label='Alterar' icon='pencil' target='tabUpdate' />
-                    <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
-                </TabsHeader>
-                <TabsContent>
-
-                </TabsContent>
-            </Tabs>
+          <Tabs>
+            <TabsHeader>
+              <TabHeader label='Listar' icon='bars' target='tabList' />
+              <TabHeader label='Incluir' icon='plus' target='tabCreate' />
+              <TabHeader label='Alterar' icon='pencil' target='tabUpdate' />
+              <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
+            </TabsHeader>
+            <TabsContent>
+              <TabContent id="tabList">
+                <h2>Lista</h2>
+              </TabContent>
+                <TabContent id="tabCreate">
+                <h2>Create</h2>
+              </TabContent>
+                <TabContent id="tabUpdate">
+                <h2>Update</h2>
+              </TabContent>
+                <TabContent id="tabDelete">
+                <h2>Delete</h2>
+              </TabContent>
+            </TabsContent>
+          </Tabs>
         </Content>
       </div>
     )
   }
 }
+
+// const mapStateToProps = state => ({tab: state.tab})
+const mapDispatchToProps = dispatch => bindActionCreators({selectTab}, dispatch)
+export default connect(null, mapDispatchToProps)(billingCycle)
--- src/common/tab/tabHeader.jsx
+++ src/common/tab/tabHeader.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
-export default class tabHeader extends Component {
+import { selectTab} from './tabAction'
+
+class tabHeader extends Component {
   render() {
-
+    const selected = this.props.tab.selected === this.props.target
     return (
-      <li>
+      <li className={selected ? 'active' : ''}>
         <a href='javascript:;'
            data-toggle='tab'
+           onClick={() => this.props.selectTab(this.props.target)}
            data-target={this.props.target}>
             <i className={`fa fa-${this.props.icon}`}></i>
             {this.props.label}
@@ -15,3 +20,6 @@ export default class tabHeader extends Component {
     )
   }
 }
+const mapStateToProps = state => ({tab: state.tab})
+const mapDispatchToProps = dispatch => bindActionCreators({selectTab}, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(tabHeader)
